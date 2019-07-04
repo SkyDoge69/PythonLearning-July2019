843f77cad74144823f54a326bcf5501bd7b6c2be
SkyDoge69 <luc2002penta@gmail.com>
2019-07-04 21:16:04 +0300

    Got familiar with dictionaries in Python.

---
+++ dicts.py
@@ -0,0 +1,21 @@
+thisdict = {
+	"name": "Genaka",
+	"age": "enough",
+	"status": "nai-golemiq"
+
+}
+
+def printGenak():
+	v1 = thisdict["name"]
+	v2 = thisdict["age"]
+	v3 = thisdict["status"]
+
+	print("The name is")
+	print(v1)
+	print("His age - >")
+	print(v2)
+	print("And toi e")
+	print(v3)
+
+
+printGenak()
